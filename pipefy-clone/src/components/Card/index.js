51552b8e98659d3b52a7b7bc2bfa0ec0064f90d1
Marcella <marcellatsangos@gmail.com>
2020-04-21 17:15:58 -0300

    Card

--- pipefy-clone/src/components/Card/index.js
+++ pipefy-clone/src/components/Card/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-import { Container } from './styles'
+import { Container, Label } from './styles'
 
 export default function Card() {
   return (
-    <Container />
+    <Container>
+      <header>
+        <Label color='#7159c1' />
+      </header>
+      <p>Fazer a migração completa do servidor</p>
+      <img src='https://api.adorable.io/avatars/48/abott@adorable.pngCopy to Clipboard' alt='' />
+    </Container>
   )
 }
--- pipefy-clone/src/components/List/index.js
+++ pipefy-clone/src/components/List/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 
+import { MdAdd } from 'react-icons/md'
+
+import Card from '../Card'
+
 import { Container } from './styles'
 
 export default function List() {
@@ -8,9 +12,17 @@ export default function List() {
       <header>
         <h2>Tarefas</h2>
         <button type='button'>
-
+          <MdAdd size={24} color='#FFF' />
         </button>
       </header>
+
+      <ul>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </ul>
     </Container>
   )
 }
