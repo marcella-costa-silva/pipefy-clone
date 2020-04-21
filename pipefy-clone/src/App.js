fb0d51378120ee3563fe45cea71a0231c2d7b06a
Marcella <marcellatsangos@gmail.com>
2020-04-21 16:37:33 -0300

    Header

--- pipefy-clone/src/App.js
+++ pipefy-clone/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
+import GlobalStyle from './styles/global'
+import Header from './components/Header'
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <Header />
+      <GlobalStyle />
+    </>
   )
 }
 
---
+++ pipefy-clone/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+export default function Header() {
+  return (
+   <Container>
+      Pipefy Clone
+   </Container> 
+  )
+}
